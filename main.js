3570c053da9be989f55aef8154200986f527c30c
LegatusNewt <kennywu1005@gmail.com>
2016-08-22 20:43:28 -0400

    Woops

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var roleRepairer = require('role.repairer');
 var roleWallRepairer = require('role.wallRepairer');
+var assignSource = require('assignSource');
 
 
 module.exports.loop = function () {
