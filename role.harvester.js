1e508271e5557c75c2f668954103a9ebcf74d037
LegatusNewt <kennywu1005@gmail.com>
2016-08-22 20:38:29 -0400

    Spliting up harvesters

--- role.harvester.js
+++ role.harvester.js
@@ -47,7 +47,10 @@ module.exports = {
         else {
             // find closest source
             var source = creep.pos.findClosestByPath(FIND_SOURCES);
-            creep.memory.sourceID = source.id;
+            if(source !== null)
+            {
+                creep.memory.sourceID = source.id.toString;
+            }
             // try to harvest energy, if the source is not in range
             if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 // move towards the source
